813734092bb4e11fa83b43c79ab8666d241389f3
haelmorn <haelmorn@gmail.com>
2019-09-30 23:50:51 +0200

    Added calendar url

--- fantasyf1/config.py
+++ fantasyf1/config.py
@@ -3,8 +3,8 @@ import os
 # Use local url for local testing and os var for deployment PLZ
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = "postgresql:///f1db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = "postgresql:///f1db"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
